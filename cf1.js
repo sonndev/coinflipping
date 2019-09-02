45d6a5d302a8bdb19d4571f06814ebc9c3cd0ed3
RikkeC <rc@rikkecarlsson.dk>
2019-09-02 12:20:36 +0200

    added new html file

---
+++ cf1.js
@@ -0,0 +1,80 @@
+//Rikke Louise Carlsson
+
+'use strict';
+
+let tails = 0;
+let heads = 0;
+let flip;
+
+flip = Math.random();
+flip = Math.floor(flip * 2 + 1);
+if (flip === 1)
+    tails++;
+else
+    heads++;
+
+flip = Math.random();
+flip = Math.floor(flip * 2 + 1);
+if (flip === 1)
+    tails++;
+else
+    heads++;
+
+flip = Math.random();
+flip = Math.floor(flip * 2 + 1);
+if (flip === 1)
+    tails++;
+else
+    heads++;
+
+flip = Math.random();
+flip = Math.floor(flip * 2 + 1);
+if (flip === 1)
+    tails++;
+else
+    heads++;
+
+flip = Math.random();
+flip = Math.floor(flip * 2 + 1);
+if (flip === 1)
+    tails++;
+else
+    heads++;
+
+flip = Math.random();
+flip = Math.floor(flip * 2 + 1);
+if (flip === 1)
+    tails++;
+else
+    heads++;
+
+flip = Math.random();
+flip = Math.floor(flip * 2 + 1);
+if (flip === 1)
+    tails++;
+else
+    heads++;
+
+flip = Math.random();
+flip = Math.floor(flip * 2 + 1);
+if (flip === 1)
+    tails++;
+else
+    heads++;
+
+flip = Math.random();
+flip = Math.floor(flip * 2 + 1);
+if (flip === 1)
+    tails++;
+else
+    heads++;
+
+flip = Math.random();
+flip = Math.floor(flip * 2 + 1);
+if (flip === 1)
+    tails++;
+else
+    heads++;
+
+console.log('heads: ' + heads);
+console.log('tails: ' + tails);
